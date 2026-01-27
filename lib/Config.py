df9fdac4e5968777ff6dcb73ca16f8c3fb52bf7b
Jason <jasonmohjs@gmail.com>
2026-01-27 15:31:04 +0800

    fix 2: load dot_env for windows compat

--- lib/Config.py
+++ lib/Config.py
@@ -2,13 +2,13 @@ import os
 from dotenv import load_dotenv
 import datetime
 
-load_dotenv()
+load_dotenv('utf-8')
 
 ROOT_URI = os.getenv("ROOT_URI")
 BASE_DIR = os.getcwd()
 AUTHENTICATION_URI = f"{ROOT_URI}/auth/userpass"
 USERNAME = os.getenv("USERNAME", "your_username")
-PASSWORD = repr(os.getenv("PASSWORD", "your_password")).strip("'\"")
+PASSWORD = repr(os.getenv("PASSWORD","your_password")).strip("'\"")
 EARLIEST_DATE = os.getenv("EARLIEST_DATE")
 ANALYST_LEVEL = os.getenv("ANALYST_LEVEL", "L1")
 
